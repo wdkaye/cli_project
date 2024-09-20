3a6f7c42f8e7a1db8eba314a3a33d0f5583860c4
Warren Kaye <wkaye@rds.com>
2024-09-20 16:16:13 -0700

    Up to date as of middle of class, Week 4

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { MatIconModule } from  '@angular/material/icon';
+import { MatToolbar } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterOutlet, 
+    RouterLink, 
+    MatIconModule,
+    MatToolbar
+  ],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
