c8800487b8176f4c561f186ba95bc618c2bac211
Warren Kaye <wkaye@rds.com>
2024-09-20 16:05:59 -0700

    Initial commit, Week 4 middle-of-class.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'cli_project';
+
+export class AppComponent  {
 }
+
+
+//export class AppComponent {
+//  title = 'cli_project';
+//  public forecasts: WeatherForecast[] = [];
+//}
